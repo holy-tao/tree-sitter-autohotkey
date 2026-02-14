ccebe4e8567838a2e961b981de1ce0b5d36b8414
Tao Beloney <taorcb@gmail.com>
2026-02-14 14:53:52 -0600

    Refactor: fix operator precedence
    Refactored the operator grammar to fix precedence issues and added some
    new tests for operators. Restructured to use precedence constants for
    clarity and maintainability.
    
    See: https://www.autohotkey.com/docs/v2/Variables.htm#operators

--- grammar.js
+++ grammar.js
@@ -9,11 +9,37 @@
 
 
 // precedence constants - default when unset is 0, higher number = higher precedence.
+// Based on https://www.autohotkey.com/docs/v2/Variables.htm#operators
 
-/**
- * Keyword-level precedence. E.g 'static' matches scope_identifier before identifier
- */
-const KEYWORD = 5;
+// Precedence levels (from lowest to highest)
+const PREC = {
+  COMMA: -20,                // Comma operator (lowest)
+  FAT_ARROW_FUNCTION : -10,  // () => expr (not implemented)
+  ASSIGNMENT: 0,             // :=, +=, -=, etc.
+  TERNARY: 10,               // ?: (not yet implemented)
+  LOGICAL_OR: 20,            // ||, or
+  LOGICAL_AND: 30,           // &&, and
+  LOGICAL_NOT: 40,           // not (verbal NOT operator)
+  CASE_INSENSITIVE: 50,      // is (type comparison)
+  REGEX_MATCH: 60,           // ~= (regex match)
+  INEQUALITY: 70,            // !=, !==
+  EQUALITY: 80,              // =, ==
+  RELATIONAL: 90,            // <, >, <=, >=
+  CONCAT: 100,               // . (explicit string concatenation, not implemented)
+  BITWISE_OR: 110,           // |
+  BITWISE_XOR: 120,          // ^
+  BITWISE_AND: 130,          // &
+  SHIFT: 140,                // <<, >>, >>> (not yet implemented)
+  ADDITIVE: 150,             // +, -
+  MULTIPLICATIVE: 160,       // *, /, //
+  EXPONENT: 170,             // ** (not yet implemented)
+  PREFIX: 180,               // ++, --, unary +, -, !, ~, &
+  POSTFIX: 190,              // ++, --
+  MAYBE: 200,                // ? (not yet implemented))
+  MEMBER_ACCESS: 210,        // a.b (not yet implemented)
+  DEREFERENCE: 220,          // %expr% (not yet implemented)
+  KEYWORD: 9999,             // Keywords should match before other identifiers
+};
 
 /**
  * NOTE: The closest thing AutoHotkey has to a formal specification is the "Language" doc: https://www.autohotkey.com/docs/v2/Language.htm
@@ -42,7 +68,7 @@ export default grammar({
     _primary_expression: $ => choice(
       $.literal,
       $.identifier,
-      seq("(", $.expression_sequence, ")"),
+      prec(3, seq("(", $.expression_sequence, ")")),
       $._pairwise_operation
     ),
 
@@ -54,24 +80,26 @@ export default grammar({
       seq("&", $._primary_expression),    // VarRefs get hairy, &(a := fn()) and &%"var"% are legal
       $.prefix_operation,
       $.postfix_operation,
+      $.verbal_not_operation,
     ),
 
-    expression_sequence: $ => seq(
+    expression_sequence: $ => prec.left(PREC.COMMA, seq(
       $.single_expression,
       repeat(seq(",", $.single_expression))
-    ),
+    )),
 
     // FIXME some declarations are contextually illegal - you can't delcare local variables in the auto-execute
-    // section, for example.
+    // section, for example. We may not be able to detect those with pure grammar rules
     variable_declaration: $ => seq(
       $.scope_identifier,
       $.identifier
     ),
 
+    //#region Operators
     // TODO left-hand-side can be an accessor like outer.inner but scope identifier can't precede accessor
     // TODO rhs can be literal or statement - probably primary expression
     // TODO make these fields
-    assignment_operation: $ => prec.left(0, seq(
+    assignment_operation: $ => prec.right(PREC.ASSIGNMENT, seq(
       field("left", $.single_expression),
       $.assignment_operator,
       field("right", $.single_expression)
@@ -79,36 +107,134 @@ export default grammar({
 
     // Any expression like left <op> right (e.g. 2 + 2, true != false)
     _pairwise_operation: $ => choice(
-      $.math_operation,
-      $.comparison_operation
+      $.additive_operation,
+      $.multiplicative_operation,
+      $.relational_operation,
+      $.equality_operation,
+      $.inequality_operation,
+      $.regex_match_operation,
+      $.case_insensitive_operation,
+      $.logical_and_operation,
+      $.logical_or_operation,
+      $.bitwise_and_operation,
+      $.bitwise_xor_operation,
+      $.bitwise_or_operation
     ),
 
     // Postfix increment/decrement
-    postfix_operation: $ => prec.left(2, seq(
+    postfix_operation: $ => prec.left(PREC.POSTFIX, seq(
       field("operand", $._primary_expression),
       field("operator", choice("++", "--"))
     )),
 
-    // Prefix increment/decrement
-    prefix_operation: $ => prec.right(1, seq(
-      field("operator", choice("++", "--")),
+    // Prefix increment/decrement and high-precedence unary operators
+    prefix_operation: $ => prec.right(PREC.PREFIX, seq(
+      field("operator", choice(
+        "++", "--",     // prefix increment/decrement
+        "!", "~",       // logical NOT, bitwise NOT
+        "+", "-"        // unary plus, unary minus
+      )),
       field("operand", choice($._primary_expression, $.postfix_operation))
     )),
 
-    math_operation: $ => prec.left(0, seq(
+    // Verbal NOT operator (lower precedence than !)
+    verbal_not_operation: $ => prec.right(PREC.LOGICAL_NOT, seq(
+      field("operator", token(prec(PREC.KEYWORD, ci('not')))),
+      field("operand", $.single_expression)
+    )),
+
+    additive_operation: $ => prec.left(PREC.ADDITIVE, seq(
       field("left", $.single_expression),
-      $.math_operator,
+      field("operator", choice("+", "-")),
       field("right", $.single_expression)
     )),
 
-    comparison_operation: $ => prec.left(0, seq(
+    multiplicative_operation: $ => prec.left(PREC.MULTIPLICATIVE, seq(
       field("left", $.single_expression),
-      $.boolean_comparison_operator,
+      field("operator", choice("*", "/", "//")),
       field("right", $.single_expression)
     )),
 
-    identifier: $ => /[a-zA-Z_][a-zA-Z0-9_]*/,
+    relational_operation: $ => prec.left(PREC.RELATIONAL, seq(
+      field("left", $.single_expression),
+      field("operator", choice("<", ">", "<=", ">=")),
+      field("right", $.single_expression)
+    )),
 
+    equality_operation: $ => prec.left(PREC.EQUALITY, seq(
+      field("left", $.single_expression),
+      field("operator", choice("=", "==")),
+      field("right", $.single_expression)
+    )),
+
+    inequality_operation: $ => prec.left(PREC.INEQUALITY, seq(
+      field("left", $.single_expression),
+      field("operator", choice("!=", "!==")),
+      field("right", $.single_expression)
+    )),
+
+    regex_match_operation: $ => prec.left(PREC.REGEX_MATCH, seq(
+      field("left", $.single_expression),
+      field("operator", "~="),
+      field("right", $.single_expression)
+    )),
+
+    case_insensitive_operation: $ => prec.left(PREC.CASE_INSENSITIVE, seq(
+      field("left", $.single_expression),
+      field("operator", token(prec(PREC.KEYWORD, ci(' is ')))),
+      field("right", $.single_expression)
+    )),
+
+    logical_and_operation: $ => prec.left(PREC.LOGICAL_AND, seq(
+      field("left", $.single_expression),
+      field("operator", choice("&&", token(prec(PREC.KEYWORD, ci('and'))))),
+      field("right", $.single_expression)
+    )),
+
+    logical_or_operation: $ => prec.left(PREC.LOGICAL_OR, seq(
+      field("left", $.single_expression),
+      field("operator", choice("||", token(prec(PREC.KEYWORD, ci('or'))))),
+      field("right", $.single_expression)
+    )),
+
+    bitwise_and_operation: $ => prec.left(PREC.BITWISE_AND, seq(
+      field("left", $.single_expression),
+      field("operator", "&"),
+      field("right", $.single_expression)
+    )),
+
+    bitwise_xor_operation: $ => prec.left(PREC.BITWISE_XOR, seq(
+      field("left", $.single_expression),
+      field("operator", "^"),
+      field("right", $.single_expression)
+    )),
+
+    bitwise_or_operation: $ => prec.left(PREC.BITWISE_OR, seq(
+      field("left", $.single_expression),
+      field("operator", "|"),
+      field("right", $.single_expression)
+    )),
+
+    assignment_operator: $ => 
+      choice( ":=", "+=", "-=", "*=", "/=", "//=", ".=", "|=", "&=", "^=", ">>=", "<<=", ">>>="),
+
+    // "is must have whitespace, others are optional"
+    comparison_operator: $ => choice(">", "<", ">=", "<=", "=", "==", "!=", "!==", "~=", ci(' is ')),
+
+    unary_operator: $ => choice("~", "+", "-", "!"),
+
+    math_operator: $ => choice("+", "-", "*", "/", "//"),
+
+    bitwise_operator: $ => choice("&", "|", "^"),
+
+    boolean_comparison_operator: $ => token(
+      prec(PREC.KEYWORD, 
+        choice("&&", ci('and'), "||", ci('or')
+    ))),
+
+    //#endregion
+
+    //#region Literals
     literal: $ => choice(
       $.numeric_literal,
       $.boolean_literal,
@@ -123,7 +249,7 @@ export default grammar({
     ),
 
     boolean_literal: $ => token(
-      prec(KEYWORD, choice(ci('true'), ci('false')))
+      prec(PREC.KEYWORD, choice(ci('true'), ci('false')))
     ),
 
     // TODO multiline string literals - https://www.autohotkey.com/docs/v2/Scripts.htm#continuation
@@ -131,6 +257,10 @@ export default grammar({
       seq('"', repeat($._double_quote_str_char), '"'),
       seq("'", repeat($._single_quote_str_char), "'")
     ),
+
+    //#endregion
+
+    identifier: $ => /[a-zA-Z_][a-zA-Z0-9_]*/,
 
     // backtick plus any non-whitespace character - ahk doesn't actually require that the backtick is
     // followed by a character that creates an escape sequence, though it will skip the backtick if it
@@ -149,39 +279,25 @@ export default grammar({
 
     // Note: not strictly "modifiers" because declaring one can switch a function into assume-local/global mode
     scope_identifier: $ => token(
-      prec(KEYWORD, choice(
+      prec(PREC.KEYWORD, choice(
         ci('static'),
         ci('local'),
         ci('global')
     ))),
 
-    assignment_operator: $ => 
-      choice( ":=", "+=", "-=", "*=", "/=", "//=", ".=", "|=", "&=", "^=", ">>=", "<<=", ">>>="),
-
-    // "is must have whitespace, others are optional"
-    comparison_operator: $ => choice(">", "<", ">=", "<=", "=", "==", "!=", "!==", ci(' is ')),
-
-    unary_operator: $ => choice("~", "+", "-", "!"),
-
-    math_operator: $ => choice("+", "-", "*", "/", "//"),
-
-    bitwise_operator: $ => choice("&", "|", "^"),
-
-    boolean_comparison_operator: $ => token(
-      prec(KEYWORD, 
-        choice("&&", ci('and'), "||", ci('or')
-    ))),
-
     // Control flow keywords
-    _if: $ => token(prec(KEYWORD, ci('if'))),
-    _else: $ => token(prec(KEYWORD, ci('else'))),
-    _while: $ => token(prec(KEYWORD, ci('while'))),
-    _for: $ => token(prec(KEYWORD, ci('for'))),
-    _in: $ => token(prec(KEYWORD, ci('in'))),
-    _loop: $ => token(prec(KEYWORD, ci('loop'))),
-    _until: $ => token(prec(KEYWORD, ci('until'))),
+    _if: $ => token(prec(PREC.KEYWORD, ci('if'))),
+    _else: $ => token(prec(PREC.KEYWORD, ci('else'))),
+    _while: $ => token(prec(PREC.KEYWORD, ci('while'))),
+    _for: $ => token(prec(PREC.KEYWORD, ci('for'))),
+    _in: $ => token(prec(PREC.KEYWORD, ci('in'))),
+    _loop: $ => token(prec(PREC.KEYWORD, ci('loop'))),
+    _until: $ => token(prec(PREC.KEYWORD, ci('until'))),
 
-    unset: $ => token(prec(KEYWORD, ci('unset'))),
+    unset: $ => token(prec(PREC.KEYWORD, ci('unset'))),
+
+    // Reserved keyword (not currently used as operator in v2, but reserved)
+    _contains: $ => token(prec(PREC.KEYWORD, ci('contains'))),
 
     // Directives - we should match these to prevent errors, but for our purposes we don't actually
     // care about their contents, except for HotIf
@@ -189,7 +305,7 @@ export default grammar({
     // #Include, #HotIf, etc
     directive: $ => seq("#", $.directive_identifier, $.anything),
 
-    directive_identifier: $ => token(prec(KEYWORD, choice(
+    directive_identifier: $ => token(prec(PREC.KEYWORD, choice(
       ci("clipboardtimeout"), ci("dllload"), ci("errstdout"), ci("requires"), ci("hotif"), ci("hotiftimeout"),
       ci("hotstring"), ci("include"), ci("includeagain"), ci("inputlevel"), ci("usehook"), ci("maxthreads")
     ))),
